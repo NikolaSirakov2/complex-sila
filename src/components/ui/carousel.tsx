41e319eb07e9f4b6b17a65df6d0db99e91968587
Nikola Sirakov <nikolasirakov1@gmail.com>
2025-10-01 20:18:47 +0300

    Update CarouselControl styles for improved visual consistency, changing button background and icon color.

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -132,13 +132,13 @@ const CarouselControl = ({
 }: CarouselControlProps) => {
   return (
     <button
-      className={`w-10 h-10 flex items-center mx-2 justify-center bg-neutral-200 dark:bg-neutral-800 border-3 border-transparent rounded-full focus:border-[#6D64F7] focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
+      className={`w-10 h-10 flex items-center mx-2 justify-center bg-pink-500 hover:bg-pink-600 border-3 border-transparent rounded-full focus:border-pink-400 focus:outline-none hover:-translate-y-0.5 active:translate-y-0.5 transition duration-200 ${
         type === "previous" ? "rotate-180" : ""
       }`}
       title={title}
       onClick={handleClick}
     >
-      <IconArrowNarrowRight className="text-neutral-600 dark:text-neutral-200" />
+      <IconArrowNarrowRight className="text-white" />
     </button>
   );
 };
